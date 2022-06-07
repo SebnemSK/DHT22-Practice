ab47d77a11d705138d627412ef022008be662a2a
SebnemSK <skuzulugil@gmail.com>
2022-06-07 18:06:09 -0400

    Snippets with operators

---
+++ opeartors.R
@@ -0,0 +1,31 @@
+# Mod is the remainder of a division
+
+5 %% 2
+# gives you 1
+
+# integer division (no decimal points)
+5 %/% 2
+
+T & T
+T & F
+F & T
+F & F
+
+T | T
+T | F
+F | T
+F | F
+
+1:5 -> nums
+
+2 %in% nums
+
+library(magrittr)
+# long chain of calculations
+a <- 1:5
+b <- sqrt(a)
+
+# piped version
+1:5 %>%
+  sqrt()
+  
