683e57182205349b7df4f4aaebe98df4a1ec4d47
SebnemSK <skuzulugil@gmail.com>
2022-07-07 00:24:22 -0400

    Minor corrections

--- clean_weather.R
+++ clean_weather.R
@@ -1,5 +1,7 @@
 #' The dataset used in this demo came from this excellent course 
 #' in data cleaning hosted on Datacamp (https://www.datacamp.com/courses/cleaning-data-in-r)
+#' Downloaded in May 2021
+
 library("dplyr")
 library('tidyr')
 
@@ -34,25 +36,25 @@ weather <-
 #' Column names X1-X31 represent days of the month. 
 #' They should be values of a variable called day.
 #' pivot_longer() the columns X1:X31 as day
-
+w2 <-
 
 
 #' Another common symptom of messy data: values are variable names. 
 #' Values in the measure column should be variables (column names) 
 #' in our dataset. pivot_wider() the measure column
-
+w3 <- 
 
 # Remove X's from day column
 
 
 # Unite the year, month, and day columns
-
+w4 <-
 
 # Convert date column to proper date format using lubridates's ymd()
 
 
 # Clean up empty rows, if there are any
-
+w5 <-
 
 # See what happens if we try to convert PrecipitationIn to numeric. Get rid of T values
 
@@ -61,18 +63,25 @@ weather <-
 
 
 # Convert characters to numeric from CloudCover:WindDirDegrees
-
+w6 <- 
+  
 # Find rows with values greater than 100 for Max.Humidity
 
+  
 # Fix the error. What is the assumption?
 
+  
 # Find rows with values less than 0 for Mean.VisibilityMiles
 
+  
 # Fix the error. What is the assumption?
+
 
 # Count missing values
 
+
 # Look at histogram for  Min.TemperatureF
+
 
 # Compare to histogram for Mean.TemperatureF
 
