e3f7de3857ba959004c8587b5ec5c72adb6a9288
SebnemSK <skuzulugil@gmail.com>
2022-07-15 15:50:25 -0400

    Add smoother line

--- ggplot_2.R
+++ ggplot_2.R
@@ -5,6 +5,15 @@ ggplot(iris, aes(x = Sepal.Length, y = Petal.Length))+
   geom_point()
 
 # Add color to points
-# Basic plot
 ggplot(iris, aes(x = Sepal.Length, y = Petal.Length))+
   geom_point(aes(col=Species))
+
+# Add smoother line
+ggplot(iris, aes(x = Sepal.Length, y = Petal.Length))+
+  geom_point(aes(col=Species)) +
+  geom_smooth(col="orange")
+
+# Add species specific smoothers + lm method
+ggplot(iris, aes(x = Sepal.Length, y = Petal.Length))+
+  geom_point(aes(col=Species))+
+  geom_smooth(aes(col=Species), method="lm")
